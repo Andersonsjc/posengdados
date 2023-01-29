b22e2d9499f43b3ef97e6df0818c73428816cb7c
Andersonsjc <anderson.siqueira@gmail.com>
2023-01-29 17:35:24 -0300

    Correct bucket

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 #Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
-    buket = "terraform-anderson-igti-edc"
+    bucket = "terraform-anderson-igti-edc"
     key = "state/igti/edc/mod1/terraform.tf"
     region = "us-east-1"
   }
