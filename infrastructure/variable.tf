0f82e846aacbb17074dd8cd5c61c9303d66181dc
Andersonsjc <anderson.siqueira@gmail.com>
2023-01-29 20:41:37 -0300

    muda o nome da função lambda

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -13,3 +13,19 @@ variable "ambiente" {
 variable "numero_conta" {
   default = "511442505751"
 }
+
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMRaovivo"
+}
+
+variable "key_pair_name" {
+  default = "anderson-igti-teste"
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-4cef5427"
+}
+
+variable "vpc_id" {
+  default = "vpc-d724b4bc"
+}
